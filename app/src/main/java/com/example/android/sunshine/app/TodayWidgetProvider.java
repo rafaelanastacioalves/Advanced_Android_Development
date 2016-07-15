eaeb0007ade7c74f93fc5182881206aa7903d8cf
Rafael Anastacio Alves <rafaelanastacioalves@gmail.com>
2016-07-15 03:13:35 -0300

    Minhas alterações

---
+++ app/src/main/java/com/example/android/sunshine/app/TodayWidgetProvider.java
@@ -0,0 +1,15 @@
+package com.example.android.sunshine.app;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by rafaelanastacioalves on 7/15/16.
+ */
+public class TodayWidgetProvider extends AppWidgetPro vider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
