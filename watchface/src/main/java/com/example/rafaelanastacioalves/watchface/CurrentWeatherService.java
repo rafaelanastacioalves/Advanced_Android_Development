42eba7bd0ff8c777340fc9c5cf7d17e35f39df70
Rafael Anastacio Alves <rafaelanastacioalves@gmail.com>
2016-10-12 12:08:03 -0300

    Implements service to receive asset from device

--- watchface/src/main/java/com/example/rafaelanastacioalves/watchface/CurrentWeatherService.java
+++ watchface/src/main/java/com/example/rafaelanastacioalves/watchface/CurrentWeatherService.java
@@ -1,16 +1,100 @@
 package com.example.rafaelanastacioalves.watchface;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
-public class CurrentWeatherService extends Service {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Asset;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.WearableListenerService;
+
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
+public class CurrentWeatherService extends WearableListenerService implements DataApi.DataListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+    private static final long TIMEOUT_MS = 1000 * 10;
+    private final String LOG_TAG = getClass().getSimpleName();
+    private GoogleApiClient mGoogleApiClient;
+
     public CurrentWeatherService() {
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public void onCreate() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        for (DataEvent event : dataEvents) {
+            if (event.getType() == DataEvent.TYPE_CHANGED &&
+                    event.getDataItem().getUri().getPath().equals("/image")) {
+                DataMapItem dataMapItem = DataMapItem.fromDataItem(event.getDataItem());
+                Asset profileAsset = dataMapItem.getDataMap().getAsset("profileImage");
+                Bitmap bitmap = loadBitmapFromAsset(profileAsset);
+                // Do something with the bitmap
+            }
+        }
+    }
+
+    public Bitmap loadBitmapFromAsset(Asset asset) {
+        if (asset == null) {
+            throw new IllegalArgumentException("Asset must be non-null");
+        }
+        ConnectionResult result =
+                mGoogleApiClient.blockingConnect(TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        if (!result.isSuccess()) {
+            return null;
+        }
+        // convert asset into a file descriptor and block until it's ready
+        InputStream assetInputStream = Wearable.DataApi.getFdForAsset(
+                mGoogleApiClient, asset).await().getInputStream();
+        mGoogleApiClient.disconnect();
+
+        if (assetInputStream == null) {
+            Log.w(LOG_TAG, "Requested an unknown Asset.");
+            return null;
+        }
+        // decode the stream into a bitmap
+        return BitmapFactory.decodeStream(assetInputStream);
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+//        TODO Implement something meaningfull to user
+        Log.i(LOG_TAG, "onConnected (To Device) " );
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+//        TODO Implement something meaningfull to user
+        Log.i(LOG_TAG, "onConnection Suspended (To Device) " );
+
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+//        TODO Implement something meaningfull to user
+        Log.i(LOG_TAG, "onConnection Failed (To Device) " );
+
     }
 }
